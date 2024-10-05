2b8553aedad03fcac5050030646b2ace853d5109
WasimSamirArsad <wasimakhtar12091@gmail.com>
2024-10-05 21:08:39 +0530

    structure of project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name="sms spam classifier",
+    version="0.0.1",
+    author="wasim akhtar",
+    author_email="wasimakhtar12091@gmail.com",
+    packages=find_packages(),
+    install_requirements="requirements.txt"
+)
+
